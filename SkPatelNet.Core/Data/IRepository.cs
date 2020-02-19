1f70624ada212ebc22c2399f937013c67c10efb3
Sachin Patel <sachin@skpatel.net>
2020-02-19 09:58:28 +0530

    Repositry interface added, customer entity class added

---
+++ SkPatelNet.Core/Data/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkPatelNet.Core.Data
+{
+    public interface IRepository<T> where T:BaseEntity
+    {
+        void Add(T entity);
+        void Edit(T entity);
+        void Delete(T entity);
+        IQueryable<T> Data { get; set; }
+    }
+}
---
+++ SkPatelNet.Core/Domain/Customers/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkPatelNet.Core.Domain.Customers
+{
+    public partial class Customer:BaseEntity
+    {
+        public Guid CustomerGuid { get; set; } = Guid.NewGuid();
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
