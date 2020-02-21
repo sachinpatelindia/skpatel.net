3875befc8f75ce2d5a6cd74c987aca52988d24e3
Sachin Patel <sachin@skpatel.net>
2020-02-21 21:09:33 +0530

    custom startup class added

---
+++ SkPatelNet.Web.Framework/Infrastructure/SkPatelDbStartup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SkPatelNet.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkPatelNet.Web.Framework.Infrastructure
+{
+    public class SkPatelDbStartup : ISkPatelStartup
+    {
+        public int Order => 2000;
+
+        public void Configure(IApplicationBuilder applicationBuilder)
+        {
+            // throw new NotImplementedException();
+        }
+
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            // throw new NotImplementedException();
+        }
+    }
+}
---
+++ SkPatelNet.Web.Framework/Infrastructure/SkPatelStartup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SkPatelNet.Core.Infrastructure;
+
+namespace SkPatelNet.Web.Framework.Infrastructure
+{
+    public class SkPatelStartup : ISkPatelStartup
+    {
+        public int Order => 1000;
+
+        public void Configure(IApplicationBuilder applicationBuilder)
+        {
+            // throw new NotImplementedException();
+        }
+
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            /// throw new NotImplementedException();
+        }
+    }
+}
