8ef8daebf646a3325ef3c460464bac59892415f4
Sachin Patel <sachin@skpatel.net>
2020-02-24 09:26:43 +0530

    Blog service added

---
+++ SkPatelNet.Services/Blogs/BlogService.cs
@@ -0,0 +1,50 @@
+﻿using SkPatelNet.Core.Domain.Blogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkPatelNet.Services.Blogs
+{
+    public partial class BlogService : IBlogService
+    {
+        public void DeleteBlogPost(BlogPost blogPost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<BlogPost> GetAllBlogPostTags(bool showHiddedn = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BlogPost GetBlogPostById(int blogPostId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<BlogPost> GetBlogPostsByIds(int[] blogPostIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<BlogPost> GetPostsByDate(IList<BlogPost> blogPosts, DateTime dateFrom, DateTime dateTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertBlogPost(BlogPost blogPost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> ParseTags(BlogPost blogPost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBlogPost(BlogPost blogPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SkPatelNet.Services/Blogs/IBlogService.cs
@@ -0,0 +1,19 @@
+﻿using SkPatelNet.Core.Domain.Blogs;
+using System;
+using System.Collections.Generic;
+
+namespace SkPatelNet.Services.Blogs
+{
+    public partial interface IBlogService
+    {
+        void InsertBlogPost(BlogPost blogPost);
+        void UpdateBlogPost(BlogPost blogPost);
+        void DeleteBlogPost(BlogPost blogPost);
+        BlogPost GetBlogPostById(int blogPostId);
+        IList<BlogPost> GetBlogPostsByIds(int[] blogPostIds);
+        IList<BlogPost> GetAllBlogPostTags(bool showHiddedn=true);
+        IList<BlogPost> GetPostsByDate(IList<BlogPost> blogPosts , DateTime dateFrom, DateTime dateTo);
+        IList<string> ParseTags(BlogPost blogPost);
+
+    }
+}
